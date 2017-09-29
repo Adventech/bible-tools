cae00a003e99555d0321b8cad26965d01ef28313
PrJared <30575911+PrJared@users.noreply.github.com>
2017-09-29 11:18:38 +1000

    added books of bible

---
+++ bibles/en/asv/books/64.js
@@ -0,0 +1,23 @@
+var book = {
+	"name": "3 John",
+	"numChapters": 1,
+	"chapters": {
+		"1": {
+			"1": "<sup class=\"versenum\">1 </sup>The elder unto Gaius the beloved, whom I love in truth.",
+			"2": "<sup class=\"versenum\">2 </sup>Beloved, I pray that in all things thou mayest prosper and be in health, even as thy soul prospereth. ",
+			"3": "<sup class=\"versenum\">3 </sup>For I rejoiced greatly, when brethren came and bare witness unto thy truth, even as thou walkest in truth. ",
+			"4": "<sup class=\"versenum\">4 </sup>Greater joy have I none than this, to hear of my children walking in the truth.",
+			"5": "<sup class=\"versenum\">5 </sup>Beloved, thou doest a faithful work in whatsoever thou doest toward them that are brethren and strangers withal; ",
+			"6": "<sup class=\"versenum\">6 </sup>who bare witness to thy love before the church: whom thou wilt do well to set forward on their journey worthily of God: ",
+			"7": "<sup class=\"versenum\">7 </sup>because that for the sake of the Name they went forth, taking nothing of the Gentiles. ",
+			"8": "<sup class=\"versenum\">8 </sup>We therefore ought to welcome such, that we may be fellow-workers for the truth.",
+			"9": "<sup class=\"versenum\">9 </sup>I wrote somewhat unto the church: but Diotrephes, who loveth to have the preeminence among them, receiveth us not. ",
+			"10": "<sup class=\"versenum\">10 </sup>Therefore, if I come, I will bring to remembrance his works which he doeth, prating against us with wicked words: and not content therewith, neither doth he himself receive the brethren, and them that would he forbiddeth and casteth <i class=\"trans-change\">them</i> out of the church. ",
+			"11": "<sup class=\"versenum\">11 </sup>Beloved, imitate not that which is evil, but that which is good. He that doeth good is of God: he that doeth evil hath not seen God. ",
+			"12": "<sup class=\"versenum\">12 </sup>Demetrius hath the witness of all <i class=\"trans-change\">men</i>, and of the truth itself: yea, we also bear witness; and thou knowest that our witness is true.",
+			"13": "<sup class=\"versenum\">13 </sup>I had many things to write unto thee, but I am unwilling to write <i class=\"trans-change\">them</i> to thee with ink and pen: ",
+			"14": "<sup class=\"versenum\">14 </sup>but I hope shortly to see thee, and we shall speak face to face. Peace <i class=\"trans-change\">be</i> unto thee. The friends salute thee. Salute the friends by name."
+		}
+	}
+};
+module.exports = book;
