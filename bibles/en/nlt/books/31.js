f5f43c802c0cfc9526831d98ee1e8e9d9548c2d3
PrJared <30575911+PrJared@users.noreply.github.com>
2017-09-29 11:56:22 +1000

    Add files via upload

---
+++ bibles/en/nlt/books/31.js
@@ -0,0 +1,30 @@
+var book = {
+	"name": "Obadiah",
+	"numChapters": 1,
+	"chapters": {
+		"1": {
+			"1": "<sup class=\"versenum\">1 </sup>This is the vision that the Sovereign <span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span> revealed to Obadiah concerning the land of Edom.We have heard a message from the <span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span>that an ambassador was sent to the nations to say,“Get ready, everyone!Let’s assemble our armies and attack Edom!”",
+			"2": "<sup class=\"versenum\">2 </sup>The <span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span> says to Edom,“I will cut you down to size among the nations;you will be greatly despised.",
+			"3": "<sup class=\"versenum\">3 </sup>You have been deceived by your own pridebecause you live in a rock fortressand make your home high in the mountains.‘Who can ever reach us way up here?’you ask boastfully.",
+			"4": "<sup class=\"versenum\">4 </sup>But even if you soar as high as eaglesand build your nest among the stars,I will bring you crashing down,”says the <span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span>.",
+			"5": "<sup class=\"versenum\">5 </sup>“If thieves came at night and robbed you(what a disaster awaits you!),they would not take everything.Those who harvest grapesalways leave a few for the poor.But your enemies will wipe you out completely!",
+			"6": "<sup class=\"versenum\">6 </sup>Every nook and cranny of Edomwill be searched and looted.Every treasure will be found and taken.",
+			"7": "<sup class=\"versenum\">7 </sup>“All your allies will turn against you.They will help to chase you from your land.They will promise you peacewhile plotting to deceive and destroy you.Your trusted friends will set traps for you,and you won’t even know about it.",
+			"8": "<sup class=\"versenum\">8 </sup>At that time not a single wise personwill be left in the whole land of Edom,”says the <span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span>.“For on the mountains of EdomI will destroy everyone who has understanding.",
+			"9": "<sup class=\"versenum\">9 </sup>The mightiest warriors of Temanwill be terrified,and everyone on the mountains of Edomwill be cut down in the slaughter.",
+			"10": "<sup class=\"versenum\">10 </sup>“Because of the violence you didto your close relatives in Israel,you will be filled with shameand destroyed forever.",
+			"11": "<sup class=\"versenum\">11 </sup>When they were invaded,you stood aloof, refusing to help them.Foreign invaders carried off their wealthand cast lots to divide up Jerusalem,but you acted like one of Israel’s enemies.",
+			"12": "<sup class=\"versenum\">12 </sup>“You should not have gloatedwhen they exiled your relatives to distant lands.You should not have rejoicedwhen the people of Judah suffered such misfortune.You should not have spoken arrogantlyin that terrible time of trouble.",
+			"13": "<sup class=\"versenum\">13 </sup>You should not have plundered the land of Israelwhen they were suffering such calamity.You should not have gloated over their destructionwhen they were suffering such calamity.You should not have seized their wealthwhen they were suffering such calamity.",
+			"14": "<sup class=\"versenum\">14 </sup>You should not have stood at the crossroads,killing those who tried to escape.You should not have captured the survivorsand handed them over in their terrible time of trouble.",
+			"15": "<sup class=\"versenum\">15 </sup>“The day is near when I, the <span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span>,will judge all godless nations!As you have done to Israel,so it will be done to you.All your evil deedswill fall back on your own heads.",
+			"16": "<sup class=\"versenum\">16 </sup>Just as you swallowed up my peopleon my holy mountain,so you and the surrounding nationswill swallow the punishment I pour out on you.Yes, all you nations will drink and staggerand disappear from history.",
+			"17": "<sup class=\"versenum\">17 </sup>“But Jerusalem will become a refuge for those who escape;it will be a holy place.And the people of Israel will come backto reclaim their inheritance.",
+			"18": "<sup class=\"versenum\">18 </sup>The people of Israel will be a raging fire,and Edom a field of dry stubble.The descendants of Joseph will be a flameroaring across the field, devouring everything.There will be no survivors in Edom.I, the <span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span>, have spoken!",
+			"19": "<sup class=\"versenum\">19 </sup>“Then my people living in the Negevwill occupy the mountains of Edom.Those living in the foothills of Judahwill possess the Philistine plainsand take over the fields of Ephraim and Samaria.And the people of Benjaminwill occupy the land of Gilead.",
+			"20": "<sup class=\"versenum\">20 </sup>The exiles of Israel will return to their landand occupy the Phoenician coast as far north as Zarephath.The captives from Jerusalem exiled in the northwill return home and resettle the towns of the Negev.",
+			"21": "<sup class=\"versenum\">21 </sup>Those who have been rescued will go up to Mount Zion in Jerusalemto rule over the mountains of Edom.And the <span style=\"font-variant: small-caps\" class=\"small-caps\">Lord</span> himself will be king!”"
+		}
+	}
+};
+module.exports = book;
---
+++ bibles/en/nlt/books/57.js
@@ -0,0 +1,34 @@
+var book = {
+	"name": "Philemon",
+	"numChapters": 1,
+	"chapters": {
+		"1": {
+			"1": "<sup class=\"versenum\">1 </sup>This letter is from Paul, a prisoner for preaching the Good News about Christ Jesus, and from our brother Timothy.I am writing to Philemon, our beloved co-worker,",
+			"2": "<sup class=\"versenum\">2 </sup>and to our sister Apphia, and to our fellow soldier Archippus, and to the church that meets in your house.",
+			"3": "<sup class=\"versenum\">3 </sup>May God our Father and the Lord Jesus Christ give you grace and peace.",
+			"4": "<sup class=\"versenum\">4 </sup>I always thank my God when I pray for you, Philemon,",
+			"5": "<sup class=\"versenum\">5 </sup>because I keep hearing about your faith in the Lord Jesus and your love for all of God’s people.",
+			"6": "<sup class=\"versenum\">6 </sup>And I am praying that you will put into action the generosity that comes from your faith as you understand and experience all the good things we have in Christ.",
+			"7": "<sup class=\"versenum\">7 </sup>Your love has given me much joy and comfort, my brother, for your kindness has often refreshed the hearts of God’s people.",
+			"8": "<sup class=\"versenum\">8 </sup>That is why I am boldly asking a favor of you. I could demand it in the name of Christ because it is the right thing for you to do.",
+			"9": "<sup class=\"versenum\">9 </sup>But because of our love, I prefer simply to ask you. Consider this as a request from me—Paul, an old man and now also a prisoner for the sake of Christ Jesus.",
+			"10": "<sup class=\"versenum\">10 </sup>I appeal to you to show kindness to my child, Onesimus. I became his father in the faith while here in prison.",
+			"11": "<sup class=\"versenum\">11 </sup>Onesimus hasn’t been of much use to you in the past, but now he is very useful to both of us.",
+			"12": "<sup class=\"versenum\">12 </sup>I am sending him back to you, and with him comes my own heart.",
+			"13": "<sup class=\"versenum\">13 </sup>I wanted to keep him here with me while I am in these chains for preaching the Good News, and he would have helped me on your behalf.",
+			"14": "<sup class=\"versenum\">14 </sup>But I didn’t want to do anything without your consent. I wanted you to help because you were willing, not because you were forced.",
+			"15": "<sup class=\"versenum\">15 </sup>It seems you lost Onesimus for a little while so that you could have him back forever.",
+			"16": "<sup class=\"versenum\">16 </sup>He is no longer like a slave to you. He is more than a slave, for he is a beloved brother, especially to me. Now he will mean much more to you, both as a man and as a brother in the Lord.",
+			"17": "<sup class=\"versenum\">17 </sup>So if you consider me your partner, welcome him as you would welcome me.",
+			"18": "<sup class=\"versenum\">18 </sup>If he has wronged you in any way or owes you anything, charge it to me.",
+			"19": "<sup class=\"versenum\">19 </sup><span style=\"font-variant: small-caps\" class=\"small-caps\">I, PAUL, WRITE THIS WITH MY OWN HAND: I WILL REPAY IT. AND I WON’T MENTION THAT YOU OWE ME YOUR VERY SOUL</span>!",
+			"20": "<sup class=\"versenum\">20 </sup>Yes, my brother, please do me this favor for the Lord’s sake. Give me this encouragement in Christ.",
+			"21": "<sup class=\"versenum\">21 </sup>I am confident as I write this letter that you will do what I ask and even more!",
+			"22": "<sup class=\"versenum\">22 </sup>One more thing—please prepare a guest room for me, for I am hoping that God will answer your prayers and let me return to you soon.",
+			"23": "<sup class=\"versenum\">23 </sup>Epaphras, my fellow prisoner in Christ Jesus, sends you his greetings.",
+			"24": "<sup class=\"versenum\">24 </sup>So do Mark, Aristarchus, Demas, and Luke, my co-workers.",
+			"25": "<sup class=\"versenum\">25 </sup>May the grace of the Lord Jesus Christ be with your spirit."
+		}
+	}
+};
+module.exports = book;
